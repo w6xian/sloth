9154d90d52f6df6e22a17a7b12c903a6f70a0ed4
Leo.prc <921717181@qq.com>
2026-02-21 21:56:39 +0800

    Update i.go

--- bucket/i.go
+++ bucket/i.go
@@ -7,7 +7,9 @@ import (
 )
 
 type IChannel interface {
+	// Call performs an RPC call with the given method and arguments, returning the response or an error.
 	Call(ctx context.Context, header message.Header, mtd string, args ...[]byte) ([]byte, error)
+	// Push sends a message to the channel without expecting a response.
 	Push(ctx context.Context, msg *message.Msg) error
 	ReplySuccess(id string, data []byte) error
 	ReplyError(id string, err []byte) error
