0fd333dbf77659d28a8c2f41058ba98afbf1c854
Leo <todayliu@hotmail.com>
2026-05-10 20:26:08 +0800

    Update channel_server.go

--- v2/nrpc/wsocket/channel_server.go
+++ v2/nrpc/wsocket/channel_server.go
@@ -284,7 +284,7 @@ func (c *WsChannelServer) ReplyError(id string, err []byte) error {
 // 服务器调用客户端方法
 func (ch *WsChannelServer) Call(ctx context.Context, header message.Header, mtd string, args ...[]byte) ([]byte, error) {
 
-	fmt.Println("--------------channel_server.go")
+	// fmt.Println("--------------channel_server.go")
 	ch.Lock.Lock()
 	defer ch.Lock.Unlock()
 	ch.log(logger.Debug, "Call mtd:%s, args:%v", mtd, args)
